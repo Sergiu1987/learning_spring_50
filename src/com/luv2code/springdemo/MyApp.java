58afa3baef2263fb3e3906b4cbf74a41910c36d6
Olgutz <sergiu.siscovschi@gmail.com>
2019-07-10 12:07:25 +0300

    initial commit

---
+++ src/com/luv2code/springdemo/MyApp.java
@@ -0,0 +1,17 @@
+package com.luv2code.springdemo;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// create the object
+		Coach theCoach_Track = new TrackCoach();
+		Coach theCoach_Baseball = new BaseballCoach();
+
+		// use the object
+		System.out.println(theCoach_Track.getDailyWorkOut());
+		System.out.println(theCoach_Baseball.getDailyWorkOut());
+	}
+
+}
