cd3e390c39e9e1ebfd9386a8422bd57dc5b6efd2
Olgutz <sergiu.siscovschi@gmail.com>
2019-07-11 17:47:05 +0300

    initial commit

--- src/com/luv2code/springdemo/CricketCoach.java
+++ src/com/luv2code/springdemo/CricketCoach.java
@@ -2,18 +2,37 @@ package com.luv2code.springdemo;
 
 public class CricketCoach implements Coach {
     
+    private String team;
+    private String emailAddress;
+    
     private FortuneService fortuneService;
     
     public CricketCoach() {
         System.out.println("CricketCOach: inside no-args constructor");
     }
     
+    public void setTeam(String team) {
+        System.out.println("CricketCoach: inside setter method - setTeam");
+        this.team = team;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        System.out.println("CricketCoach: inside setter method - setEmailAddress");
+        this.emailAddress = emailAddress;
+    }
+
     public void setFortuneService(FortuneService fortuneService) {
         System.out.println("CricketCoach: inside setter method - setFortuneService");
         this.fortuneService = fortuneService;
     }
-
-
 
     @Override
     public String getDailyWorkOut() {
